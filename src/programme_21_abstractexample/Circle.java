2609f2577f981740ec5b73351bd38e8494d143b5
paryankpatel <paryank18us@gmail.com>
2023-04-07 15:42:39 +0100

    My homework week 4

---
+++ src/programme_21_abstractexample/Circle.java
@@ -0,0 +1,11 @@
+package programme_21_abstractexample;
+
+import java.awt.*;
+
+public class Circle extends Shape {
+    public void draw() {
+        System.out.println("drawing circle");
+    }
+}
+
+
---
+++ src/programme_21_abstractexample/Rectangle.java
@@ -0,0 +1,10 @@
+package programme_21_abstractexample;
+
+import java.awt.*;
+
+public class Rectangle extends Shape {
+    public void draw() {
+        System.out.println("drawing rectangle");
+    }
+}
+
